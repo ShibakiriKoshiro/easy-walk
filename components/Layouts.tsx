bcc1081c9b33e33940a4f209afd9546f201e8606
ShibakiriKoshiro <olive070719@gmail.com>
2021-08-15 19:04:56 +0900

    fix: Sidebar(key)

--- components/Layouts.tsx
+++ components/Layouts.tsx
@@ -1,4 +1,5 @@
 import { Dialog, Transition } from '@headlessui/react';
+import { MenuIcon, SearchIcon } from '@heroicons/react/solid';
 import Link from 'next/link';
 import React, { Fragment, useState } from 'react';
 import Sidebar from './Sidebar';
@@ -12,39 +13,26 @@ const Layouts = () => {
 
   return (
     <>
-      <div className="flex items-center w-full h-16 bg-blue-300">
-        <nav className="flex container items-center w-full">
+      <div className="fixed w-full h-16 bg-blue-300 z-10">
+        <nav className="flex w-full h-full container mx-auto items-center">
           <div className="text-left">
             <button onClick={changeDrower}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clipRule="evenodd"
-                />
-              </svg>
+              <MenuIcon className="h-6 w-6" />
             </button>
           </div>
-          <div className="w-full mr-auto">
+          <div className="ml-auto">
             <Link href="/search">
               <a>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                    clipRule="evenodd"
-                  />
-                </svg>
+                <SearchIcon className="h-6 w-6" />
+              </a>
+            </Link>
+          </div>
+          <div className="ml-6">
+            <Link href="/login">
+              <a>
+                <div className="bg-blue-600 py-1 px-2 shadow rounded-sm text-white">
+                  <p>ログイン</p>
+                </div>
               </a>
             </Link>
           </div>
@@ -81,17 +69,6 @@ const Layouts = () => {
                 leaveTo="-translate-x-full"
               >
                 <div className="relative w-screen max-w-xs">
-                  <Transition.Child
-                    as={Fragment}
-                    enter="ease-in-out duration-500"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="ease-in-out duration-500"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                  >
-                    <div className="absolute top-0 right-0 -ml-8 pt-4 pr-2 flex sm:-ml-10 sm:pr-4"></div>
-                  </Transition.Child>
                   <div className="h-full flex flex-col bg-white shadow-xl overflow-y-scroll">
                     <div className="overflow-y-scroll">
                       <Sidebar />
