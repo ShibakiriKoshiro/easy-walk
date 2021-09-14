2d16922c704942e964a4ea6b9aabe40b80ee4f3d
ShibakiriKoshiro <olive070719@gmail.com>
2021-09-15 02:11:27 +0900

    feat(plan)

--- pages/plan.tsx
+++ pages/plan.tsx
@@ -1,4 +1,4 @@
-import { PlusIcon } from '@heroicons/react/solid';
+import { NewspaperIcon } from '@heroicons/react/solid';
 import React from 'react';
 import Heading from '../components/Heading';
 import TextLink from '../components/TextLink';
@@ -9,22 +9,47 @@ const Plan = () => {
       <Heading />
       <div className="container">
         <p className="text-lg mt-12">月額課金</p>
-        <div className="sm:grid sm:grid-cols-2  xl:grid-cols-3 gap-4 mt-3 mb-2">
+        <div className="sm:grid sm:grid-cols-2 gap-4 mt-3 mb-2">
           <a>
-            <div className="block sm:flex w-full px-2 py-4 rounded hover:shadowrounded border border-gray-300 hover:border-0 hover:bg-red-100 hover:shadow">
-              <div className="w-full sm:w-3/4">
-                <p className="font-bold">プレミアムプラン</p>
-                <p className="mt-2 text-gray-600">観光に関する質問し放題。</p>
-              </div>
-              <div className="w-full sm:w-1/4">
-                <div className="flex sm:block items-center">
-                  <div className="w-1/2 sm:w-full">
-                    <PlusIcon className="h-12 w-12" />
-                  </div>
-                  <div className="w-1/2 sm:w-full">
-                    <p className="mt-6 font-bold inline">100円/月</p>
-                  </div>
+            <div className="w-full px-2 py-4 rounded hover:shadowrounded border border-gray-300 hover:border-0 hover:bg-red-100 hover:shadow">
+              <div className="flex items-center">
+                <div className="w-3/4">
+                  <p className="font-bold">プレミアムプラン</p>
+                  <p className="text-gray-600">観光に関する質問し放題。</p>
                 </div>
+                <div className="w-1/4">
+                  <NewspaperIcon className="h-12 w-12" />
+                </div>
+              </div>
+              <div className="ml-auto w-1/4 mt-3">
+                <p className="font-semibold text-lg">100円/月</p>
+              </div>
+            </div>
+          </a>
+        </div>
+        <TextLink>使用方法、注意事項等はこちら</TextLink>
+      </div>
+      <div className="container">
+        <p className="text-lg mt-12">イベント</p>
+        <div className="sm:grid sm:grid-cols-2 gap-4 mt-3 mb-2">
+          <a>
+            <div className="w-full px-2 py-4 rounded hover:shadowrounded border border-gray-300 hover:border-0 hover:bg-red-100 hover:shadow">
+              <div className="flex items-center">
+                <div className="w-3/4">
+                  <p className="font-bold">バルチケット</p>
+                  <p className="text-gray-600">
+                    3枚つづりのチケットになっています。
+                  </p>
+                  <p className="text-gray-600">
+                    Easy Walk_尾道の加盟店で使用可能です。
+                  </p>
+                </div>
+                <div className="w-1/4">
+                  <NewspaperIcon className="h-12 w-12" />
+                </div>
+              </div>
+              <div className="ml-auto w-1/4 mt-3">
+                <p className="font-semibold text-lg">2499円/月</p>
               </div>
             </div>
           </a>
