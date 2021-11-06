953e26a0e0dd02b0b68d2854fdf5cf45acb8e286
DevFature01 <towhy.llc@gmail.com>
2021-11-06 18:37:41 +0900

    fix(mypage): 現状

--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import { CameraIcon, PhotographIcon } from '@heroicons/react/solid';
+import { CameraIcon } from '@heroicons/react/solid';
 import Cropper from 'cropperjs';
 import 'cropperjs/dist/cropper.css';
 import { doc, getDoc, setDoc } from 'firebase/firestore';
@@ -46,6 +46,9 @@ const Mypage = () => {
 
   // クロップ対象のファイルを管理
   const [targetFile, setTargetFile] = useState<Blob | null>();
+
+  // アップロードボタン
+  const [uploadImage, SetUploadImage] = useState<boolean>(false);
 
   // クロップ対象の画像をセット
   const setImageToCropper = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -107,7 +110,8 @@ const Mypage = () => {
         merge: true,
       }
     ).then(() => {
-      alert('アップロード完了');
+      SetUploadImage(false);
+      alert('保存完了');
     });
   };
 
@@ -117,8 +121,8 @@ const Mypage = () => {
         <p className="font-bold">プロフィール</p>
         <div>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="">
-              <label className="inline-block mb-4 cursor-pointer relative">
+            <div className="flex items-center">
+              <div className="mt-6 flex">
                 {preview ? (
                   // eslint-disable-next-line @next/next/no-img-element
                   <img
@@ -129,14 +133,33 @@ const Mypage = () => {
                 ) : (
                   <div className="w-20 h-20 rounded-full overflow-hidden border bg-gray-400"></div>
                 )}
-
-                <input
-                  className="hidden"
-                  type="file"
-                  onChange={setImageToCropper}
-                />
-                <PhotographIcon className="absolute left-1/2 top-1/2 transform opacity-60 -translate-x-1/2 -translate-y-1/2 w-10 h-10 text-white" />
-              </label>
+                <label
+                  htmlFor="avatar"
+                  className="h-full mt-auto -ml-6 cursor-pointer"
+                >
+                  <input
+                    onChange={setImageToCropper}
+                    id="avatar"
+                    type="file"
+                    className="hidden"
+                  />
+                  <CameraIcon
+                    className="h-10 w-10"
+                    fill="none"
+                    stroke="currentColor"
+                  />
+                </label>
+                {uploadImage && (
+                  <div className="mt-10 ml-12">
+                    <button
+                      className="px-2 py-1 shadow-xl rounded bg-blue-700 text-white"
+                      onClick={uploadAvatar}
+                    >
+                      アップロード
+                    </button>
+                  </div>
+                )}
+              </div>
             </div>
             <Modal
               isOpen={!!targetFile}
@@ -144,7 +167,7 @@ const Mypage = () => {
               onRequestClose={() => setTargetFile(null)}
               contentLabel="Example Modal"
             >
-              <h2 className="font-bold text-2xl mb-6">クロップ</h2>
+              <h2 className="font-bold text-2xl mb-6 mt-16">クロップ</h2>
 
               <div className="max-w-sm pb-4 border-b mb-4">
                 <img id="image" className="block w-full" alt="" />
@@ -152,6 +175,7 @@ const Mypage = () => {
 
               <button
                 className="px-4 py-3 shadow rounded bg-gray-700 text-white"
+                type="submit"
                 onClick={() => {
                   // プレビューステートにクロッピング結果を格納
                   const croppedImage = cropper
@@ -163,46 +187,14 @@ const Mypage = () => {
 
                   // プレビューステートにセット
                   setPreview(croppedImage);
-
                   // ダイヤログを閉じるためにクロップターゲットを空にする
                   setTargetFile(null);
+                  SetUploadImage(true);
                 }}
               >
                 クロップする
               </button>
             </Modal>
-            <div className="mt-10">
-              <button
-                className="px-4 py-3 shadow rounded bg-gray-700 text-white"
-                onClick={uploadAvatar}
-              >
-                アップロード
-              </button>
-            </div>
-            <div>
-              <div className="mt-6 flex">
-                <span className="inline-block h-24 w-24 rounded-full overflow-hidden bg-gray-100">
-                  <svg
-                    className="h-full w-full text-gray-300"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                  </svg>
-                </span>
-                <label
-                  htmlFor="avatar"
-                  className="h-full mt-auto -ml-6 cursor-pointer"
-                >
-                  <input id="avatar" type="file" className="hidden" />
-                  <CameraIcon
-                    className="h-10 w-10"
-                    fill="none"
-                    stroke="currentColor"
-                  />
-                </label>
-              </div>
-            </div>
             <input
               placeholder="ユーザー名"
               className="block p-1 mt-6 border-gray-300 border-b-2 w-full outline-none"
@@ -216,7 +208,7 @@ const Mypage = () => {
             <input
               placeholder="リンク"
               className="block p-1 mt-6 border-gray-300 border-b-2 w-full outline-none"
-              {...register('link', { required: true })}
+              {...register('link')}
             />
             {errors.link && (
               <p className="text-center pt-3 text-yellow-600 text-base">
